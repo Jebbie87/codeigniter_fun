a1ff938212da319598f14859351ce28931938cf6
Jeffrey Chang <jeffreycj.chang@gmail.com>
2017-06-03 22:27:15 -0400

    Initial commit, finished video 1 and 2 of build a codeigniter php app youtube series

---
+++ application/views/templates/header.php
@@ -0,0 +1,27 @@
+<html>
+	<head>
+		<title>Codeigniter fun!</title>
+		<link rel="stylesheet" href="https://bootswatch.com/flatly/bootstrap.min.css">
+		<link rel="stylesheet" href="<?php echo base_url(); ?>/assets/css/style.css"
+	</head>
+	<body>
+
+	<nav class="navbar navbar-inverse">
+		<div class="container">
+			<div class="navbar-header">
+				<a class="navbar-brand" href="<?php echo base_url(); ?>">Codeignite</a>
+			</div>
+			<div id="navbar">
+				<ul class="nav navbar-nav">
+					<li><a href="<?php echo base_url(); ?>">Home</a></li>
+					<li><a href="<?php echo base_url(); ?>about">About</a></li>
+					<li><a href="<?php echo base_url(); ?>posts">Blog</a></li>
+				</ul>
+				<ul class="nav navbar-nav navbar-right">
+					<li><a href="<?php echo base_url(); ?>posts/create">Create post</a></li>
+			</div>
+		</div>
+	</nav>
+
+	<div class="container">
+
